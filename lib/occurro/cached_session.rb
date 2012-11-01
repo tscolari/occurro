775192f64fb9c13bb59f641d397b4579fce880f2
Tiago Scolari <tscolari@gmail.com>
2012-11-01 00:34:31 -0200

    Adding documentation to some methods

--- lib/occurro/cached_session.rb
+++ lib/occurro/cached_session.rb
@@ -1,10 +1,16 @@
 module Occurro
   module CachedSession
 
+    # Public: Checks if the given session already has a visit for
+    # the given model. Retuns true or false.
+    #
     def self.unique_visitor?(model, session)
       !(session[:occurro] && session[:occurro]["#{model.class.name}"] && session[:occurro]["#{model.class.name}"]["#{model.id}"])
     end
 
+    # Public: Adds the given model to the given session.
+    # Should be used once per unique_visitor.
+    #
     def self.add_cache(model, session)
       session[:occurro] ||= {}
       session[:occurro]["#{model.class.name}"] ||= {}
--- lib/occurro/tasks.rb
+++ lib/occurro/tasks.rb
@@ -21,7 +21,8 @@ module Occurro
 
     private
 
-    # PrivatE: Update counters for the period type and countable_type
+    # Private: Update counters for the period type and countable_type
+    #
     def self.update_counters(period_type, countable_type = nil)
       counters = Occurro::Counter
       counters.where(updatable_type: countable_type) if countable_type
